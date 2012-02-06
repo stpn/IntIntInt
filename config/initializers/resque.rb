f9e6675d9e498d446da0fd39f107bd634345a12a
Stepan Boltalin <stepanboltalin@172-26-15-165.dynapool.nyu.edu>
2012-02-06 17:33:02 -0500

    routes for Resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,6 @@
+require 'resque/server'
+require 'resque/status_server'
+
 ENV["REDISTOGO_URL"] ||= "redis://stpn:b8be70ed6ebfb7d0c5b248d2e4fd4ae2@perch.redistogo.com:9511/"
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
