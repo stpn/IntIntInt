eb5588ae979fb895a85eda42dfb5d23d9167ccef
Stepan Boltalin <stepanboltalin@172-26-12-156.dynapool.nyu.edu>
2012-02-26 15:55:08 -0500

    assets

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module YApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
+    
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
